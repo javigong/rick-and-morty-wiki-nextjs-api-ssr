9a37dfc5acb9adbecb6c992d7451184cb5509acc
Javier Gongora <42308135+javigong@users.noreply.github.com>
2022-11-02 00:45:57 -0700

    Adding page transitions  with framer motion

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,23 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { motion } from "framer-motion";
+import type { AppProps } from "next/app";
+import "../styles/globals.css";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <motion.div
+      key={router.route}
+      initial="pageInitial"
+      animate="pageAnimate"
+      variants={{
+        pageInitial: {
+          opacity: 0,
+        },
+        pageAnimate: {
+          opacity: 1,
+        },
+      }}
+    >
+      <Component {...pageProps} />
+    </motion.div>
+  );
 }
